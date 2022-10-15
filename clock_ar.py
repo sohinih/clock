63085fc0324fe91942d1a2b1d24e9be1fbf87de0
SOHINI HALDER <113424657+sohinih@users.noreply.github.com>
2022-10-15 13:29:59 +0530

    Update clock_ar.py

--- clock_ar.py
+++ clock_ar.py
@@ -17,8 +17,8 @@ while True:
     if hr == datetime.datetime.now().hour:
         if mi == datetime.datetime.now().minute:
             if sec == datetime.datetime.now().second:
-                toaster.show_toast("ALARM CLOCK", "WAKE UP", duration=10, threaded=True) #load a musicfile fr plybck
-                pygame.mixer.music.load("C:\\Users\\Sohini\\Desktop\\clock\\alarm_tune.mpeg") #strt plybck of music stream
-                pygame.mixer.music.play()
+                toaster.show_toast("ALARM CLOCK", "WAKE UP", duration=10, threaded=True) 
+                pygame.mixer.music.load("C:\\Users\\Sohini\\Desktop\\clock\\alarm_tune.mpeg") #load a musicfile fr plybck
+                pygame.mixer.music.play()   #strt plybck of music stream
                 break
 print("HAVE A GOOD DAY")
